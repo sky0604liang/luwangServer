beb594e030b8fd19881ceefe653d6b6372710401
liangqw <304025298@qq.com>
2019-06-26 11:51:56 +0800

    质量管理模块

---
+++ quality-management/src/main/java/com/nnmzkj/QualityManagementApplication.java
@@ -0,0 +1,11 @@
+package com.nnmzkj;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityManagementApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
