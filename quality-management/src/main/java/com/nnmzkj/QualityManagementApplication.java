1b1dbb4038f3cfa63cb83f005b111cce0320c5ab
liangqw <304025298@qq.com>
2019-06-26 14:11:07 +0800

    配置Mybatis生成插件

--- quality-management/src/main/java/com/nnmzkj/QualityManagementApplication.java
+++ quality-management/src/main/java/com/nnmzkj/QualityManagementApplication.java
@@ -1,11 +1,14 @@
 package com.nnmzkj;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.nnmzkj.dao")
 public class QualityManagementApplication {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(QualityManagementApplication.class, args);
     }
 }
